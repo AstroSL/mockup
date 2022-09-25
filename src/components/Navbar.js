947e28db1435c9ef3d55ddaf0dace8a3e1d31a3a
Sam <layzeesam@gmail.com>
2022-09-25 11:53:30 -0700

    hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import './NavbarStyles.css'
 import{FaBars, FaTimes}from 'react-icons/fa'
 
 const Navbar = () => {
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+
+
+
   return (
     <div className='header'>
         <Link to='/'><h1>Transportation Mock</h1></Link>
-        <ul className='nav-menu'>
+        <ul className={click ? 'nav-menu active ': 'nav-menu'}>
             <li>
                 <Link to ='/'>Home</Link>
             </li>
@@ -21,8 +27,13 @@ const Navbar = () => {
                 <Link to ='/map'>Map</Link>
             </li>
         </ul>
-        <div className='hamburger'>
-            <FaBars size={20} style={{color: '#fff'}}/>
+        <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: '#fff'}}/>) : (<FaBars size={20} style={{color: '#fff'}}/>)}
+
+
+
+
+            
         </div>
     </div>
   )

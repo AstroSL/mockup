d982282f46b88f4665c1505d9462c0f4993a1ff9
Sam <layzeesam@gmail.com>
2022-09-28 10:44:30 -0700

    video js/css

--- src/components/Video.js
+++ src/components/Video.js
@@ -10,15 +10,14 @@ const Video = () => {
         <video autoPlay loop muted id='video'>
             <source src={sacOne} type='video/mp4' />
         </video>
-    <div className='content'>
-        <h1>Transportation Wizard</h1>
-        <p>Mockup</p>
-    </div>
-    <div>
-        <Link to='/training' className='btn'>Training</Link>
-        <Link to='/training' className='btn'>Training</Link>    
-                
-    </div>
+        <div className='content'>
+            <h1>Transportation Wizard</h1>
+            <p>Mockup</p>
+        <div>
+             <Link to='/form' className='btn'>Form</Link>
+             <Link to='/food' className='btn'>Food</Link>    
+            </div>
+        </div>
     </div>
     )
 }
